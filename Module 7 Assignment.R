43950da90df5f695988d312d3ea8b7b8939ae710
jonathanrprogramming <35276790+jonathanrprogramming@users.noreply.github.com>
2018-02-22 17:07:44 -0500

    Add files via upload

---
+++ Module 7 Assignment.R
@@ -0,0 +1,7 @@
+install.packages("statesRcontiguous")
+shapefile_details
+.S3methods(generic.function = "statesRcontiguous")
+.S4methods(generic.function = "statesRcontiguous")
+showMethods("statesRcontiguous")
+typeof("statesRcontiguous")
+methods(generic.function = "statesRcontiguous")
